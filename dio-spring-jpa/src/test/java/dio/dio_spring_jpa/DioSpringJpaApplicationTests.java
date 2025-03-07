c6da6c77785dee3b912dc7b35b7d11648b54991e
Danieldealmeida2024 <danieldealmeidadev@outlook.com>
2025-03-07 10:43:40 -0300

    Desafio API Restful JPA

---
+++ dio-spring-jpa/src/test/java/dio/dio_spring_jpa/DioSpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.dio_spring_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioSpringJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
